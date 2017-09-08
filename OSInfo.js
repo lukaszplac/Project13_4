0786c4bc824093fa326e59544fa7e7ba44f1f2cd
lukaszplac123 <lukasz.plac@gmail.com>
2017-09-08 16:58:52 +0200

    Project 13.4. Task completed

--- OSInfo.js
+++ OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var tc = require('./modules/TimeConv.js');
 function getOsInfo() {
 	var type = os.type();
 	var release = os.release();
@@ -13,7 +14,8 @@ function getOsInfo() {
 	console.log('System:', type);
 	console.log('Release:', release);
 	console.log('CPU model:', cpu);
-	console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+	console.log('Uptime in minutes: ~', tc.convertToMin(uptime));
+	console.log('Uptime in hours: ~', tc.convertToHours(uptime));
 	console.log('User name:', userInfo.username);
 	console.log('Home dir:', userInfo.homedir);		
 }
