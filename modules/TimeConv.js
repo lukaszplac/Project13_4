b24ad4d2cde5ae778893f097b98e499010e91d44
lukaszplac123 <lukasz.plac@gmail.com>
2017-09-11 23:21:57 +0200

    Next release. Time conversion corrected. npm package created

--- modules/TimeConv.js
+++ modules/TimeConv.js
@@ -1,9 +1,14 @@
 function convertToMin(timeInSec) {
-	return (timeInSec / 60).toFixed(0) + "min";
+	var minutes = Math.floor(timeInSec / 60);
+	var seconds = Math.floor(timeInSec - (minutes * 60));   
+	return minutes + "min " + seconds + "sec";
 }
 
 function conmvertToHours(timeInSec) {
-	return (timeInSec / 3600).toFixed() + "h";
+	var hours = Math.floor(timeInSec / 3600);
+	var minutes = Math.floor((timeInSec - (hours * 3600)) / 60);
+    var seconds = Math.floor(timeInSec - (hours * 3600) - (minutes * 60));
+	return  hours + "godz " + minutes + "min " + seconds + "sec";
 }
 
 exports.convertToMin = convertToMin;
